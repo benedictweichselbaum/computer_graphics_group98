76fd5a014b7d3448b55f8cef685072ebc7dc6e17
benedictweichselbaum <b.weichselbaum@mailbox.org>
2021-11-04 16:21:35 +0100

    Add advanced framework. Works in Linux

---
+++ gcAdvanced_framework/build/src/CMakeFiles/CG_Framework.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/camera.cpp" "src/CMakeFiles/CG_Framework.dir/framework/camera.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/camera.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/cg_helper.cpp" "src/CMakeFiles/CG_Framework.dir/framework/cg_helper.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/cg_helper.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/imgui/imgui.cpp" "src/CMakeFiles/CG_Framework.dir/framework/imgui/imgui.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/imgui/imgui.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/imgui/imgui_draw.cpp" "src/CMakeFiles/CG_Framework.dir/framework/imgui/imgui_draw.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/imgui/imgui_draw.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/imgui/imgui_impl_sdl_gl3.cpp" "src/CMakeFiles/CG_Framework.dir/framework/imgui/imgui_impl_sdl_gl3.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/imgui/imgui_impl_sdl_gl3.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/imgui/imgui_orient.cpp" "src/CMakeFiles/CG_Framework.dir/framework/imgui/imgui_orient.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/imgui/imgui_orient.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/objLoader.cpp" "src/CMakeFiles/CG_Framework.dir/framework/objLoader.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/objLoader.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/pngLoader.cpp" "src/CMakeFiles/CG_Framework.dir/framework/pngLoader.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/pngLoader.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/shaderManager.cpp" "src/CMakeFiles/CG_Framework.dir/framework/shaderManager.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/shaderManager.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/framework/window.cpp" "src/CMakeFiles/CG_Framework.dir/framework/window.cpp.o" "gcc" "src/CMakeFiles/CG_Framework.dir/framework/window.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
