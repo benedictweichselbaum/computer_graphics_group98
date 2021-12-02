b2045b21c914b02ca7aec53fdf87f9c60fa87edb
benedictweichselbaum <b.weichselbaum@mailbox.org>
2021-12-02 19:47:19 +0100

    Add Exercises 7

---
+++ gcAdvanced_framework/build/src/Advanced07/CMakeFiles/Advanced07.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/Advanced07/src/cg.cpp" "src/Advanced07/CMakeFiles/Advanced07.dir/src/cg.cpp.o" "gcc" "src/Advanced07/CMakeFiles/Advanced07.dir/src/cg.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/Advanced07/src/fps_camera.cpp" "src/Advanced07/CMakeFiles/Advanced07.dir/src/fps_camera.cpp.o" "gcc" "src/Advanced07/CMakeFiles/Advanced07.dir/src/fps_camera.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/Advanced07/src/main.cpp" "src/Advanced07/CMakeFiles/Advanced07.dir/src/main.cpp.o" "gcc" "src/Advanced07/CMakeFiles/Advanced07.dir/src/main.cpp.o.d"
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/src/Advanced07/src/quaternion.cpp" "src/Advanced07/CMakeFiles/Advanced07.dir/src/quaternion.cpp.o" "gcc" "src/Advanced07/CMakeFiles/Advanced07.dir/src/quaternion.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/benedict/Documents/Master_Studium/Semester_1/Computer_Graphics/computer_graphics_group98_exercises/gcAdvanced_framework/build/src/CMakeFiles/CG_Framework.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ gcAdvanced_framework/build/src/Advanced07/CMakeFiles/Advanced07.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../../../src/Advanced07/Advanced07"
+  "../../../src/Advanced07/Advanced07.pdb"
+  "CMakeFiles/Advanced07.dir/src/cg.cpp.o"
+  "CMakeFiles/Advanced07.dir/src/cg.cpp.o.d"
+  "CMakeFiles/Advanced07.dir/src/fps_camera.cpp.o"
+  "CMakeFiles/Advanced07.dir/src/fps_camera.cpp.o.d"
+  "CMakeFiles/Advanced07.dir/src/main.cpp.o"
+  "CMakeFiles/Advanced07.dir/src/main.cpp.o.d"
+  "CMakeFiles/Advanced07.dir/src/quaternion.cpp.o"
+  "CMakeFiles/Advanced07.dir/src/quaternion.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Advanced07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
